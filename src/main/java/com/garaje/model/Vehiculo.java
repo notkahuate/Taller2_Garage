6fc9fa211a5d6e93b8793f15e7a01671c7216445
notkahuate <ksbuenors@gmail.com>
2025-09-30 23:07:46 -0500

    feat: :sparkles: creacion entidades vehiculos

--- src/main/java/com/garaje/model/Vehiculo.java
+++ src/main/java/com/garaje/model/Vehiculo.java
@@ -4,10 +4,48 @@
  */
 package com.garaje.model;
 
-/**
- *
- * @author kbuen
- */
 public class Vehiculo {
+    private int id;
+    private String placa;
+    private String marca;
+    private String modelo;
+    private String color;
+    private String propietario;
+
+    public Vehiculo() { }
+
+    public Vehiculo(int id, String placa, String marca, String modelo,
+    String color, String propietario) {
+    this.id = id;
+    this.placa = placa;
+    this.marca = marca;
+    this.modelo = modelo;
+    this.color = color;
+    this.propietario = propietario;
+    }
+
+    public int getId() { return id; }
     
+    public void setId(int id) { this.id = id; }
+    
+    public String getPlaca() { return placa; }
+ 
+    public void setPlaca(String placa) { this.placa = placa; }
+    
+    public String getMarca() { return marca; }
+  
+    public void setMarca(String marca) { this.marca = marca; }
+   
+    public String getModelo() { return modelo; }
+
+    public void setModelo(String modelo) { this.modelo = modelo; }
+
+    public String getColor() { return color; }
+   
+    public void setColor(String color) { this.color = color; }
+  
+    public String getPropietario() { return propietario; }
+   
+    public void setPropietario(String propietario) { this.propietario =
+    propietario; }
 }
